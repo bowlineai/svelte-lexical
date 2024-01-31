afab717d9038956c1782fcbaeee3e9544bf21699
George Czabania <1140167+stayradiated@users.noreply.github.com>
2024-01-31 10:24:39 -0500

    chore(v0.7.5): export nodes as a standalone file

---
+++ packages/svelte-lexical/src/nodes.ts
@@ -0,0 +1,10 @@
+import pkgrich from '@lexical/rich-text';
+const {HeadingNode, QuoteNode} = pkgrich;
+export {HeadingNode, QuoteNode}
+
+import pkglist from '@lexical/list';
+const {ListItemNode, ListNode} = pkglist;
+export {ListNode, ListItemNode}
+
+export {HorizontalRuleNode} from './core/plugins/HorizontalRuleNode';
+export {ImageNode} from './core/plugins/Image/ImageNode';
